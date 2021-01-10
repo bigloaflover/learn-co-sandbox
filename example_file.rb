c17be92898a8673559aacb357bf5ae245bb9c32e
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-10 19:42:20 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,7 @@
+person = {
+  name: "sam",
+  age: 31
+}
+
+person[:age] = 32
+#updates the hash value for age to 32
---
+++ test_file.rb
@@ -0,0 +1,8 @@
+the_beatles_members_as_an_array = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
+
+english_bands_by_city = { 
+  :liverpool => "The Beatles", 
+  :manchester => "The Smiths", 
+  :coerty => "Delia Derbyshire and the BBC Radiophonic Band", 
+  :london => "Ziggy Stardust and the spiders from mars"
+}
